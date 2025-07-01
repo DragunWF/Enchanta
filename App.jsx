97f6ab647d70609c4a78baef8ce1bc27f6315f6a
DragunDev <plarisanmarc@gmail.com>
2025-07-01 17:02:50 +0800

    Setup initial config

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <Text>Hello There!</Text>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
