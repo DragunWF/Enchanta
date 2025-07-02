78674b180ef24fa042ee431aa27e208359597983
DragunDev <plarisanmarc@gmail.com>
2025-07-02 21:06:44 +0800

    Setup initial template for bot context

--- store/botContext.tsx
+++ store/botContext.tsx
@@ -1,17 +1,39 @@
-import { createContext, useReducer, ReactNode } from "react";
+import { createContext, useState, ReactNode } from "react";
 
 interface BotContextType {
-  
+  mood: string;
+  importantFacts: string[];
+  datetime: string;
+  bondLevel: string;
+  quirkVariation: string;
 }
 
 interface BotContextProviderProps {
   children: ReactNode;
 }
 
-export const BotContext = createContext<BotContextType>({});
+export const BotContext = createContext<BotContextType>({
+  mood: "",
+  importantFacts: [],
+  datetime: "",
+  bondLevel: "",
+  quirkVariation: "",
+});
 
 function BotContextProvider({ children }: BotContextProviderProps) {
-  const value: BotContextType = {};
+  const [mood, setMood] = useState("");
+  const [importantFacts, setImportantFacts] = useState([]);
+  const [datetime, setDatetime] = useState("");
+  const [bondLevel, setBondLevel] = useState("");
+  const [quirkVariation, setQuirkVariation] = useState("");
+
+  const value: BotContextType = {
+    mood,
+    importantFacts,
+    datetime,
+    bondLevel,
+    quirkVariation,
+  };
 
   return <BotContext.Provider value={value}>{children}</BotContext.Provider>;
 }
