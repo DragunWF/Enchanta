924e73305e68cb2f20e2df34ea88d58d5e7cd6f0
DragunDev <plarisanmarc@gmail.com>
2025-07-02 12:00:04 +0800

    Update gemini.js

--- helpers/gemini.js
+++ helpers/gemini.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const API_KEY = "";
-const MODEL_NAME = "gemini-1.5-flash"; 
+// NOTE: This public key is temporary
+const API_KEY = process.env.EXPO_PUBLIC_GEMINI_API_KEY;
+const MODEL_NAME = "gemini-1.5-flash";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/${MODEL_NAME}:generateContent?key=${API_KEY}`;
 
 const geminiApi = axios.create({
