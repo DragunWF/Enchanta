d5bba6aa44b7d8d5f1d47a2336361dc08faaa907
DragunDev <92813115+DragunWF@users.noreply.github.com>
2025-07-03 01:18:32 +0800

    Update gemini.ts - update ai model

--- helpers/tools/gemini.ts
+++ helpers/tools/gemini.ts
@@ -3,12 +3,13 @@ import axios, { AxiosError } from "axios";
 /*
    Preferred Gemini Models:
    - gemini-2.0-flash-lite
-   - gemini-1.5-flash
+   - gemini-2.0-flash
+   - gemini-2.5-flash-lite-preview-06-17
 */
 
 // NOTE: This public key is temporary
 const API_KEY = process.env.EXPO_PUBLIC_GEMINI_API_KEY;
-const MODEL_NAME = "gemini-2.0-flash-lite";
+const MODEL_NAME = "gemini-2.5-flash-lite-preview-06-17";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/${MODEL_NAME}:generateContent?key=${API_KEY}`;
 
 // Define the expected response structure
