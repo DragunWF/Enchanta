8424b37ebdb4539663aac94f88967d699fb6d02d
DragunDev <plarisanmarc@gmail.com>
2025-07-03 00:49:41 +0800

    Change gemini ai model

--- helpers/tools/gemini.ts
+++ helpers/tools/gemini.ts
@@ -1,8 +1,14 @@
 import axios, { AxiosError } from "axios";
 
+/*
+   Preferred Gemini Models:
+   - gemini-2.0-flash-lite
+   - gemini-1.5-flash
+*/
+
 // NOTE: This public key is temporary
 const API_KEY = process.env.EXPO_PUBLIC_GEMINI_API_KEY;
-const MODEL_NAME = "gemini-1.5-flash";
+const MODEL_NAME = "gemini-2.0-flash-lite";
 const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/${MODEL_NAME}:generateContent?key=${API_KEY}`;
 
 // Define the expected response structure
