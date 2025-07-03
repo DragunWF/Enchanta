80e0d376bcd1fb8924161b2b0ff2d3f5d54af88a
DragunDev <plarisanmarc@gmail.com>
2025-07-03 18:45:17 +0800

    Setup initial UI layout for settings screen

--- screens/SettingsScreen.tsx
+++ screens/SettingsScreen.tsx
@@ -1,13 +1,62 @@
-import { StyleSheet, View, Text } from "react-native";
+import { useContext } from "react";
+import { StyleSheet, View, Text, TextInput, Button } from "react-native";
+
+import Title from "../components/ui/Title";
+import { ChatContext } from "../store/chatContext";
+import { BotContext } from "../store/botContext";
 
 function SettingsScreen() {
+  const chatContext = useContext(ChatContext);
+  const botContext = useContext(BotContext);
+
   return (
     <View>
-      <Text>SettingsScreen</Text>
+      <Title>Settings</Title>
+      <View style={styles.settingContainer}>
+        <Text style={styles.settingLabel}>Bot Current Mood</Text>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.textInput} placeholder="Hello" />
+          <View style={styles.button}>
+            <Button title="Save" />
+          </View>
+        </View>
+      </View>
+      <View style={styles.settingContainer}>
+        <Text style={styles.settingLabel}>Bot Quirk Variation</Text>
+        <View style={styles.inputContainer}>
+          <TextInput style={styles.textInput} placeholder="Hello" />
+          <View style={styles.button}>
+            <Button title="Save" />
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  settingContainer: {
+    marginVertical: 10,
+    marginHorizontal: 20,
+    justifyContent: "center",
+    alignItems: "flex-start",
+  },
+  settingLabel: {
+    fontFamily: "quicksand-bold",
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  inputContainer: {
+    flexDirection: "row",
+  },
+  textInput: {
+    flex: 4,
+    backgroundColor: "lightgray",
+    padding: 10,
+  },
+  button: {
+    flex: 1,
+  },
+});
 
 export default SettingsScreen;
