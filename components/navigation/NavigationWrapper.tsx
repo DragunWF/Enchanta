0a62e9e5cb98c5559d4cc2d2efadba5bea8b4dcd
DragunWF <plarisanmarc2@gmail.com>
2025-07-10 02:56:34 +0800

    Create NavigationWrapper.tsx component - for future implementation

---
+++ components/navigation/NavigationWrapper.tsx
@@ -0,0 +1,18 @@
+import { StyleSheet, View, Text } from "react-native";
+
+import CustomBackground from "../ui/CustomBackground";
+
+function NavigationWrapper() {
+  // TODO: Wrap the entire navigation component here
+  return (
+    <CustomBackground>
+      <View>
+        <Text>NavigationWrapper</Text>
+      </View>
+    </CustomBackground>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default NavigationWrapper;
