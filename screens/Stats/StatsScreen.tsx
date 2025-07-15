9e4d6318fdfe1ac202f667e77aed858d6e5cf466
DragunDev <plarisanmarc@gmail.com>
2025-07-16 00:03:14 +0800

    Create base boilerplate for stats screen

--- screens/Stats/StatsScreen.tsx
+++ screens/Stats/StatsScreen.tsx
@@ -1,13 +1,22 @@
 import { StyleSheet, View, Text } from "react-native";
 
+import Title from "../../components/ui/Title";
+import CustomBackground from "../../components/ui/CustomBackground";
+
 function StatsScreen() {
   return (
-    <View>
-      <Text>StatsScreen</Text>
-    </View>
+    <CustomBackground>
+      <View style={styles.rootContainer}>
+        <Title>Usage Statistics</Title>
+      </View>
+    </CustomBackground>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1,
+  },
+});
 
 export default StatsScreen;
